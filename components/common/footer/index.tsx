eed17796007a0aa4ffe2e09de2f22f7c0d160606
Adil Peter <adilnesmith@ymail.com>
2023-03-26 20:44:05 +0500

    feat: unit test for footer has been added

--- components/common/footer/index.tsx
+++ components/common/footer/index.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react';
 import { FooterProps } from 'lib/@types/common'
 import styles from './Footer.module.scss';
-import {COLUMN_1,COLUMN_2,COLUMN_3 } from 'lib/constants/Footer';
+import { COLUMN_1, COLUMN_2, COLUMN_3 } from 'lib/constants/Footer';
 const Footer: FC<FooterProps> = () => {
     return (
         <footer className={styles.wrapper}>
-            <div className={styles.wrapper__columns__first}>{COLUMN_1}</div>
-            <div className={styles.wrapper__columns__second}>{COLUMN_2}</div>
-            <div className={styles.wrapper__columns__third}>{COLUMN_3}</div>
+            <div data-testid="column-1" className={styles.wrapper__columns__first}>{COLUMN_1}</div>
+            <div data-testid="column-2" className={styles.wrapper__columns__second}>{COLUMN_2}</div>
+            <div data-testid="column-3" className={styles.wrapper__columns__third}>{COLUMN_3}</div>
         </footer>
     )
 }
